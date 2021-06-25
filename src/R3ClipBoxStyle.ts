367a632d5bb51285bbb21114f4efe8a8b24726c8
NeoTRAN001 <neo_tran@outlook.com>
2021-06-24 21:51:29 -0500

    feat: add new sizes

--- src/R3ClipBoxStyle.ts
+++ src/R3ClipBoxStyle.ts
@@ -1,48 +1,66 @@
 import { css } from 'lit';
 
 export default css`
-:host {
-    font-family: 'VT323', monospace;
+  :host {
+    font-family: 'Blinker', sans-serif, monospace;
     --main-size-with: #70px;
     --main-size-height: #70px;
-    --font-size: 50px;
+    --font-size: 45px;
     --main-radius: 50%;
   }
 
-    #clip {
-        background: #25282f;
-        border-radius: var(--main-radius);
-        width: var(--main-size-with);
-        height: var(--main-size-height);
-        color: white;
-        display: flex;
-        justify-content: center;
-    }
+  #clip {
+    background: #25282f;
+    border-radius: var(--main-radius);
+    width: var(--main-size-with);
+    height: var(--main-size-height);
+    color: white;
+    display: flex;
+    justify-content: center;
+  }
 
-    #clip label {
-        margin: auto;
-        font-size: var(--font-size);
-    }
+  #clip label {
+    margin: auto;
+    font-size: var(--font-size);
+  }
 
-    #clip.small {
-        --main-size-with: 50px;
-        --main-size-height: 50px;
-        --font-size: 30px;
-    }
+  #clip.supersmall {
+    --main-size-with: 30px;
+    --main-size-height: 30px;
+    --font-size: 15px;
+  }
 
-    #clip.medium {
-        --main-size-with: 70px;
-        --main-size-height: 70px;
-        --font-size: 50px;
-    }
+  #clip.small {
+    --main-size-with: 50px;
+    --main-size-height: 50px;
+    --font-size: 25px;
+  }
 
-    #clip.big {
-        --main-size-with: 100px;
-        --main-size-height: 100px;
-        --font-size: 80px;
-    }
+  #clip.medium {
+    --main-size-with: 70px;
+    --main-size-height: 70px;
+    --font-size: 45px;
+  }
 
-    #clip.square {
-        --main-radius: 10px;
-    }
+  #clip.big {
+    --main-size-with: 100px;
+    --main-size-height: 100px;
+    --font-size: 60px;
+  }
+
+  #clip.superbig {
+    --main-size-with: 150px;
+    --main-size-height: 150px;
+    --font-size: 90px;
+  }
+
+  #clip.occupy-all {
+    --main-size-with: 100%;
+    --main-size-height: 100%;
+    --font-size: 90px;
+  }
+
+  #clip.square {
+    --main-radius: 10px;
+  }
 `;
--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -1,3 +1,5 @@
+/* eslint lit/attribute-value-entities: "off" */
+/* eslint import/no-extraneous-dependencies: "off" */
 import { html, TemplateResult } from 'lit-html';
 import '../r3-clip-box.js';
 
@@ -8,7 +10,7 @@ export default {
     image: { control: 'image' },
     size: { control: 'size' },
     text: { control: 'color' },
-    variant: { control: 'variant' }
+    variant: { control: 'variant' },
   },
 };
 
@@ -31,8 +33,12 @@ const Template: Story<ArgTypes> = ({
   text = 'NA',
   variant = 'solid',
 }: ArgTypes) => html`
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=VT323&display=swap" rel="stylesheet"> 
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Blinker:wght@600&display=swap"
+    rel="stylesheet"
+  />
   <r3-clip-box
     image="${image}"
     size="${size}"
@@ -49,23 +55,35 @@ CustomTitle.args = {
   text: 'Asuna X',
 };
 
-export const BigSize = Template.bind({});
-BigSize.args = {
+export const SuperSmallSize = Template.bind({});
+SuperSmallSize.args = {
   text: 'Asuna X',
-  size: 'big'
+  size: 'supersmall',
 };
 
 export const SmallSize = Template.bind({});
 SmallSize.args = {
   text: 'Asuna X',
-  size: 'small'
+  size: 'small',
+};
+
+export const BigSize = Template.bind({});
+BigSize.args = {
+  text: 'Asuna X',
+  size: 'big',
+};
+
+export const SuperBigSize = Template.bind({});
+SuperBigSize.args = {
+  text: 'Asuna X',
+  size: 'superbig',
 };
 
 export const Square = Template.bind({});
 Square.args = {
   text: 'Asuna X',
   size: 'big',
-  variant: 'square'
+  variant: 'square',
 };
 
 export const Image = Template.bind({});
@@ -73,5 +91,28 @@ Image.args = {
   text: 'Asuna X',
   size: 'big',
   variant: 'square',
-  image: "https://avatars.githubusercontent.com/u/86213026?s=200&v=4"
+  image: 'https://avatars.githubusercontent.com/u/86213026?s=200&v=4',
+};
+
+export const SuperBigSizeSquareImage = Template.bind({});
+SuperBigSizeSquareImage.args = {
+  text: 'Asuna X',
+  size: 'superbig',
+  variant: 'square',
+  image: 'https://avatars.githubusercontent.com/u/86213026?s=200&v=4',
+};
+
+export const Size100Image = Template.bind({});
+Size100Image.args = {
+  text: 'Asuna X',
+  size: '100',
+  image: 'https://avatars.githubusercontent.com/u/86213026?s=200&v=4',
+};
+
+export const Size100SquareImage = Template.bind({});
+Size100SquareImage.args = {
+  text: 'Asuna X',
+  size: '100',
+  image: 'https://avatars.githubusercontent.com/u/86213026?s=200&v=4',
+  variant: 'square',
 };
